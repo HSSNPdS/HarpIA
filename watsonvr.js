19fcf100705a755a4bd6d0abb5e792452ba3b9e1
HSSNPdS <hssnpds@gmail.com>
2020-11-08 19:00:45 -0300

    IBM Watson VR connection

---
+++ watsonvr.js
@@ -0,0 +1,25 @@
+var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
+var fs = require('fs');
+
+var visualRecognition = new VisualRecognitionV3({
+	version: '2018-03-19',
+	iam_apikey: 'rsI3xpZYJbSovD2raH6IP4or0D8g8MXL-S2oZbPAsRYI'
+});
+
+var images_file= fs.createReadStream('./azl1.jpeg');
+var classifier_ids = ["Protótipo_133288788"];
+var threshold = 0.6;
+
+var params = {
+	images_file: images_file,
+	classifier_ids: classifier_ids,
+	threshold: threshold
+};
+
+visualRecognition.classify(params, function(err, response) {
+	if (err) { 
+		console.log(err);
+	} else {
+		console.log(JSON.stringify(response, null, 2))
+	}
+});
